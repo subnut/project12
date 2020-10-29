95f6df529e6cc5f4aa70ea8bf294a8e3a40d33ff
Subhaditya Nath <sn03.general@gmail.com>
2020-10-29 18:53:47 +0530

    Added bot

---
+++ .bot.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import sys
+
+files_to_botify = sys.argv[1:]
+for filename in files_to_botify:
+    with open(filename) as file:
+        lines = file.readlines()
+    with open(f"{filename}.prebotification", "w+") as backup_file:
+        backup_file.writelines(lines)
+    for (index, line) in enumerate(lines):
+        if line.startswith("[BOT]:"):
+            origin_filename = line.lstrip("(BOT):").strip()
+            with open(origin_filename) as origin_file:
+                contents_to_insert = origin_file.read()
+            if origin_filename.find(".py") > -1:
+                filetype = "python"
+            elif origin_filename.find(".") > -1:
+                filetype = origin_filename.split(".")[-1]
+            else:
+                filetype = ""
+            lines[index] = f"```{filetype}\n{contents_to_insert}```\n"
+    with open(filename, "w+") as file:
+        file.writelines(lines)
